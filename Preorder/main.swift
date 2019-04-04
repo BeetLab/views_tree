ead792788a2f8efd07e80d379af56782a833eb96
Никита <nikita.a.semenov@gmail.com>
2019-04-04 20:50:30 +0300

    Initial Commit

---
+++ Preorder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Preorder
+//
+//  Created by Krasa on 04/04/2019.
+//  Copyright © 2019 Nikita Semenov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
